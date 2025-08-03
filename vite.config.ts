57703395d18c94ae66dcb553df64bcad1eed4f44
twossu <nadao202@naver.com>
2025-08-03 14:57:13 +0900

    init: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+});
