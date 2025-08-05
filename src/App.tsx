8e16da2124e296b9d7d7b52659245827299a78da
winchoose <tmdxor741@naver.com>
2025-08-06 00:52:58 +0900

    init: 라우터 초기세팅 Supense 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
 
 function App() {
   return (
-    <Suspense fallback={<div>로딩 중...</div>}>
       <RouterProvider router={router} />
-    </Suspense>
   );
 }
 
