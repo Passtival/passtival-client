df66c01b0497c6e66d34e50b12ef4f9ba47d9259
winchoose <tmdxor741@naver.com>
2025-08-08 21:58:45 +0900

    init: color, font 시스템 세팅 및 global css 세팅

---
+++ src/shared/styles/global.css.ts
@@ -0,0 +1,12 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('html', {
+  fontSize: '62.5%',
+});
+
+globalStyle('body', {
+  fontFamily: "'Pretendard', sans-serif",
+  backgroundColor: '#fff',
+  minHeight: '100vh',
+  overflowX: 'hidden',
+});
