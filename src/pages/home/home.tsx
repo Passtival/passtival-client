2ae9fc13902aa0c49fc256ec404bc0cec890e9a7
winchoose <tmdxor741@naver.com>
2025-08-12 20:40:40 +0900

    init: color, font 시스템 세팅 및 global css 세팅/예시 삭제

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,5 @@
-import { fontStyles } from '@shared/styles/token/font-style';
-
 const Home = () => {
-  return <div style={fontStyles.head_eb_22}>Home</div>;
+  return <div>Home</div>;
 };
 
 export default Home;
