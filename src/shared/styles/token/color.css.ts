f8383043a3ff207567783d9e1d40f8d9a6a4eea6
winchoose <tmdxor741@naver.com>
2025-08-14 22:31:29 +0900

    init: color, font 시스템 세팅 및 global css 세팅/컬러 적용

--- src/shared/styles/token/color.css.ts
+++ src/shared/styles/token/color.css.ts
@@ -1,4 +1,15 @@
 export const color = {
   black: '#000000',
-  white: '#FFFFFF',
+  bg_white: '#FFFFFF',
+
+  main_blue: '#1876FA',
+  sub_blue: '#EFF5FF',
+  error_pink: '#FF0080',
+  success_green: '#1AD27C',
+
+  gray_200: '#F8F8F8',
+  gray_400: '#C8C8C8',
+  gray_500_40: '#636363 @40%',
+  gray_600: '#808080',
+  gray_900: '#242424',
 };
