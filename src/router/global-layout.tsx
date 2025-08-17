142e2f855cced892a58884d76b882ad4432aba78
twossu <nadao202@naver.com>
2025-08-17 20:10:02 +0900

    fix: global-layout 수정

--- src/router/global-layout.tsx
+++ src/router/global-layout.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 
-import BottomNav from '@shared/components/bottom-navigation/bottom-navigation';
-
 export default function GlobalLayout() {
   return (
     <Suspense>
       <Outlet />
-      <BottomNav />
     </Suspense>
   );
 }
--- src/router/routes/global-routes.tsx
+++ src/router/routes/global-routes.tsx
@@ -1,4 +1,3 @@
-import GlobalLayout from '../global-layout';
 import {
   BlindMatchPage,
   BoothPage,
@@ -13,41 +12,35 @@ import { routePath } from '../path';
 
 export const publicRoutes = [
   {
-    element: <GlobalLayout />,
-    children: [
-      {
-        path: routePath.LOGIN,
-        Component: LoginPage,
-      },
-      {
-        path: routePath.LOST_ITEMS,
-        Component: LostItemsPage,
-      },
-      {
-        path: routePath.HOME,
-        Component: HomePage,
-      },
-      {
-        path: routePath.BOOTH,
-        Component: BoothPage,
-      },
-      {
-        path: routePath.BLIND_MATCH,
-        Component: BlindMatchPage,
-      },
-      {
-        path: routePath.LAND,
-        Component: LandPage,
-      },
-      {
-        path: routePath.TICKET,
-        Component: TicketPage,
-      },
-      {
-        path: routePath.LOGIN_FALLBACK,
-        Component: LoginFallbackPage,
-      },
-    ],
+    path: routePath.LOGIN,
+    Component: LoginPage,
   },
-  //BottomNav를 사용하지 않는 페이지는 이곳에 넣어주세요.
+  {
+    path: routePath.LOST_ITEMS,
+    Component: LostItemsPage,
+  },
+  {
+    path: routePath.HOME,
+    Component: HomePage,
+  },
+  {
+    path: routePath.BOOTH,
+    Component: BoothPage,
+  },
+  {
+    path: routePath.BLIND_MATCH,
+    Component: BlindMatchPage,
+  },
+  {
+    path: routePath.LAND,
+    Component: LandPage,
+  },
+  {
+    path: routePath.TICKET,
+    Component: TicketPage,
+  },
+  {
+    path: routePath.LOGIN_FALLBACK,
+    Component: LoginFallbackPage,
+  },
 ];
