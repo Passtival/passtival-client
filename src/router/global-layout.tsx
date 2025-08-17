e751080b58836cced865d21643f7d3cc51baca87
twossu <nadao202@naver.com>
2025-08-17 18:21:18 +0900

    fix: global-layout 수정

--- src/router/global-layout.tsx
+++ src/router/global-layout.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
 
-import BottomNavigation from '@shared/components/bottom-navigation/bottom-navigation';
+import BottomNav from '@shared/components/bottom-navigation/bottom-navigation';
 
 export default function GlobalLayout() {
   return (
     <Suspense>
       <Outlet />
-      <BottomNavigation />
+      <BottomNav />
     </Suspense>
   );
 }
