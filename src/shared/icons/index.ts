f9c26ccbab05fec745a09193111fb2dd661e8e46
winchoose <tmdxor741@naver.com>
2025-08-20 20:47:55 +0900

    Feat: TopNavigation 컴포넌트 구현  (#48)
    * feat: TopNavigation 컴포넌트 구현
    
    * feat: TopNavigation 컴포넌트 구현
    
    * Merge branch 'develop' of https://github.com/Passtival/passtival-client into feat/top-navigation-component/#39
    
    * feat: TopNavigation 컴포넌트 구현/ handleLeftClick구현
    
    * fix: 화살표 아이콘 이름 변경
    
    ---------
    
    Co-authored-by: jeonghoon11 <jih361@naver.com>

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,6 +1,7 @@
 // (auto-generated) Do not edit manually.
 // Run `pnpm svgr` to regenerate.
 
+export { default as IcSvgArrow } from './ic_arrow';
 export { default as IcSvgBooth } from './ic_booth';
 export { default as IcSvgCalendar } from './ic_calendar';
 export { default as IcSvgCamera } from './ic_camera';
