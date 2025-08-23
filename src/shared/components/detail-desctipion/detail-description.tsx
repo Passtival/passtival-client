46768ddd3bec20f80dddb752d24e25e240184c4a
twossu <nadao202@naver.com>
2025-08-24 02:41:16 +0900

    feat: DetailDescription 완성

---
+++ src/shared/components/detail-desctipion/detail-description.tsx
@@ -0,0 +1,18 @@
+import * as styles from './detail-description.css';
+
+interface DescriptionProps {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const DetailDescription = ({ title, description }: DescriptionProps) => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.description}>{description}</p>
+    </div>
+  );
+};
+
+export default DetailDescription;
