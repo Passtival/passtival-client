3fb7617dc51569da128035a5d7290f916622e4a4
twossu <nadao202@naver.com>
2025-08-23 20:50:47 +0900

    feat: tsx 완성

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,15 @@
+import DetailHeader from '@shared/components/detail-header/detail-header';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <DetailHeader
+        subTitle="안녕하세요"
+        title="홈 화면"
+      />
+      <div style={{ backgroundColor: 'pink' }}> 오픈준비중</div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/shared/components/detail-header/detail-header.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@shared/styles/theme.css';
+
+export const cotainer = style({});
+export const subTitle = style({});
+export const title = style({});
