4df7f55f2c8c4969f2723647c7d36c7ffa3b7e11
twossu <nadao202@naver.com>
2025-08-23 22:55:29 +0900

    fix: alt 속성 추가

--- src/shared/components/detail-img/detail-img.tsx
+++ src/shared/components/detail-img/detail-img.tsx
@@ -2,13 +2,15 @@ import * as styles from './detail-img.css';
 
 interface DeatilImgProps {
   img: string;
+  alt?: string;
 }
-const DetailImg = ({ img }: DeatilImgProps) => {
+const DetailImg = ({ img, alt }: DeatilImgProps) => {
   return (
     <div className={styles.container}>
       <img
         className={styles.img}
         src={img}
+        alt={alt}
       />
     </div>
   );
