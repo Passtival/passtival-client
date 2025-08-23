aee4f52f0ed3a5bf3d9d2a9dd31229f5e6f0e153
twossu <nadao202@naver.com>
2025-08-23 22:48:49 +0900

    feat: 구현완료

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,5 @@
-import DetailImg from '@shared/components/detail-img/detail-img';
-
-import test from '../../../public/test.png';
-
 const Home = () => {
-  return (
-    <div>
-      <DetailImg img={test} />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
