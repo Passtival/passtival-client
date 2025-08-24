4dfef8f7c24100c389d4d317cbb46e6e6061e1c9
twossu <nadao202@naver.com>
2025-08-25 00:29:38 +0900

    fix: home 페이지 복구

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,34 +1,5 @@
-import Carousel from '@shared/components/carousel/carousel';
-
-import cuteImage from '../../../public/cute.jpg';
-import noWakeupImage from '../../../public/no-wakeup.jpg';
-import yupdduckImage from '../../../public/yupdduck.jpg';
-
 const Home = () => {
-  return (
-    <div>
-      <Carousel type="Apply">
-        <img src={noWakeupImage} />
-        <img src={yupdduckImage} />
-        <img src={noWakeupImage} />
-        <img src={cuteImage} />
-        <img src={yupdduckImage} />
-        <img src={noWakeupImage} />
-        <img src={cuteImage} />
-      </Carousel>
-      <Carousel type="details">
-        <img src={noWakeupImage} />
-        <img src={yupdduckImage} />
-      </Carousel>
-      <Carousel
-        type="details"
-        infinite={true}
-      >
-        <img src={noWakeupImage} />
-        <img src={yupdduckImage} />
-      </Carousel>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
