1255e8111daa3ab67603e995c5c57006dfe5b0d9
twossu <nadao202@naver.com>
2025-08-25 00:27:57 +0900

    fix: subTitle 글꼴컬러 추가

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,5 @@
-import DetailHeader from '@shared/components/detail-header/detail-header'; // DetailHeader 컴포넌트 import
-
 const Home = () => {
-  return (
-    <div>
-      {/* DetailHeader 컴포넌트 사용 */}
-      <DetailHeader
-        subTitle="2024년 5월 22일 ~ 5월 25일" // 원하는 subTitle 입력
-        title="2024 해양 축제" // 원하는 title 입력
-      />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
