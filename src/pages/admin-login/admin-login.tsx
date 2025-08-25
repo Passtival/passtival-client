9e5d2f124b28886e877691661503657403e4b80d
twossu <nadao202@naver.com>
2025-08-25 23:24:12 +0900

    fix: @pages/admin-login 파일 위치 수정

--- src/pages/admin-login/admin-login.tsx
+++ src/pages/admin-login/admin-login.tsx
@@ -3,17 +3,17 @@ import { useState } from 'react';
 import Button from '@shared/components/button/button';
 import Input from '@shared/components/input/input';
 
-import * as styles from '../lost-items/admin-login.css';
+import * as styles from './admin-login.css';
 
 const AdminLogin = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div>
       <p className={styles.title}>관리자 로그인</p>
       <div className={styles.loginContainer}>
         <Input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="비밀번호"
         />
         <Button
