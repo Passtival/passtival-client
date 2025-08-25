458e76112315cc471c98ac450a2e1ffa7e43c808
h2st0ry <yeonhee040512@gmail.com>
2025-08-25 18:04:01 +0900

    fix: checkbox 우측 arrow 아이콘에 대한 rotate 추가

--- src/shared/components/checkbox/checkbox.css.ts
+++ src/shared/components/checkbox/checkbox.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 import { themeVars } from '@shared/styles/theme.css';
 
@@ -11,21 +12,30 @@ export const checkboxWrapper = style({
   cursor: 'pointer',
 });
 
-export const checkbox = style({
-  display: 'flex',
-  width: '2rem',
-  height: '2rem',
-  border: `1px solid ${themeVars.color.main_blue}`,
-  borderRadius: '4px',
-  backgroundColor: themeVars.color.bg_white,
-  position: 'relative',
-  alignItems: 'center',
-  justifyContent: 'center',
-  transition: 'all 0.2s ease',
-});
-
-export const checked = style({
-  backgroundColor: themeVars.color.main_blue,
+export const checkbox = recipe({
+  base: {
+    display: 'flex',
+    width: '2rem',
+    height: '2rem',
+    border: `1px solid ${themeVars.color.main_blue}`,
+    borderRadius: '4px',
+    backgroundColor: themeVars.color.bg_white,
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: 'all 0.2s ease',
+  },
+  variants: {
+    checked: {
+      false: {}, // 기본값 유지
+      true: {
+        backgroundColor: themeVars.color.main_blue,
+      },
+    },
+  },
+  defaultVariants: {
+    checked: false,
+  },
 });
 
 export const checkboxLeft = style({
@@ -35,10 +45,8 @@ export const checkboxLeft = style({
   cursor: 'pointer',
 });
 
-export const checkIcon = style({
-  width: '1.4rem',
-  height: '1.4rem',
-  color: themeVars.color.bg_white,
+export const arrowIcon = style({
+  transform: 'rotate(180deg)',
 });
 
 export const labelText = style({
