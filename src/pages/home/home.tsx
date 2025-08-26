9d3422c9588405a13a0f293e8b436bfc6e51868d
twossu <nadao202@naver.com>
2025-08-26 17:09:55 +0900

    fix: home페이지 복구

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,37 +1,5 @@
-import Tab from '@shared/components/tab/tab';
-
-const tabData = [
-  { id: 1, content: '부스' },
-  { id: 2, content: '메뉴' },
-  { id: 3, content: '체험 활동' },
-];
-
 const Home = () => {
-  return (
-    <Tab.Container initialValue={tabData[0].content}>
-      <Tab.List>
-        {tabData.map((tab) => (
-          <Tab.Item
-            key={tab.content}
-            value={tab.content}
-          >
-            {tab.content}
-          </Tab.Item>
-        ))}
-      </Tab.List>
-
-      <Tab.Panels>
-        {tabData.map((tab) => (
-          <Tab.Panel
-            key={tab.content}
-            value={tab.content}
-          >
-            {tab.content}입니다
-          </Tab.Panel>
-        ))}
-      </Tab.Panels>
-    </Tab.Container>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
