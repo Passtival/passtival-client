d860252064212b2569117aad7fd93766fdc26597
twossu <nadao202@naver.com>
2025-08-27 03:51:55 +0900

    feat: 응모권 페이지 구현

---
+++ src/pages/ticket/ticket.css.ts
@@ -0,0 +1,44 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@shared/styles';
+
+export const headercontainer = style({
+  width: '100%',
+  height: '100%',
+  padding: '1.3rem 2.4rem 1rem 2.4rem',
+  color: themeVars.color.gray_900,
+});
+export const header = style({
+  ...themeVars.fontStyles.title_b_22,
+});
+
+export const subheader = style({
+  ...themeVars.fontStyles.button_r_12,
+});
+
+export const container = style({
+  textAlign: 'center',
+});
+
+export const product = style({
+  ...themeVars.fontStyles.title_b_16,
+  color: themeVars.color.gray_900,
+  padding: '0.8rem 0 0.7rem 0',
+});
+export const productName = style({
+  ...themeVars.fontStyles.title_b_16,
+  color: themeVars.color.main_blue,
+  padding: '1rem 0 2.3rem 0',
+});
+export const input = style({
+  display: 'grid',
+  justifyItems: 'center',
+  padding: '0.4rem 0 1.6rem 0',
+  gap: '0.5rem',
+});
+export const button = style({ padding: '1.2rem 0 0.2rem 0' });
+export const message = style({
+  ...themeVars.fontStyles.caption2_m_12,
+  color: themeVars.color.gray_900,
+  padding: '0.2rem 0 ',
+});
--- src/pages/ticket/ticket.tsx
+++ src/pages/ticket/ticket.tsx
@@ -1,5 +1,56 @@
+import { useState } from 'react';
+
+import Button from '@shared/components/button/button';
+import Input from '@shared/components/input/input';
+import Thumbnail from '@shared/components/Thumbnail/Thumbnail';
+
+import * as styles from './ticket.css';
+
 const Ticket = () => {
-  return <div>Ticket</div>;
+  const [name, setName] = useState('');
+  const [studentNumber, setStudentNumber] = useState('');
+  const [key, SetKey] = useState('');
+
+  return (
+    <>
+      <div className={styles.headercontainer}>
+        <p className={styles.header}>프리미엄 응모권</p>
+        <p className={styles.subheader}>
+          축제 기간 중 한 번만 응모할 수 있습니다.
+        </p>
+      </div>
+      <div className={styles.container}>
+        <p className={styles.product}>당첨 상품</p>
+        <Thumbnail
+          src=""
+          alt=""
+          type="circle"
+        />
+        <p className={styles.productName}>에어팟 프로</p>
+        <div className={styles.input}>
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="이름"
+          />
+          <Input
+            value={studentNumber}
+            onChange={(e) => setStudentNumber(e.target.value)}
+            placeholder="학번"
+          />{' '}
+          <Input
+            value={key}
+            onChange={(e) => SetKey(e.target.value)}
+            placeholder="인증키"
+          />
+        </div>
+        <Button children="로그인" />
+        <p className={styles.message}>
+          일차별 행사 모두 참여시 전체 응모권 제공!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Ticket;
