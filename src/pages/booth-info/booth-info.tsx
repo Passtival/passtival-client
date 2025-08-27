2cb6f525b4aa923ef0460e84293658b85e34c018
twossu <nadao202@naver.com>
2025-08-27 20:59:07 +0900

    feat: booth-info.tsx 완성

---
+++ src/pages/booth-info/booth-info.tsx
@@ -0,0 +1,40 @@
+import DetailDescription from '@shared/components/detail-desctipion/detail-description';
+import DetailInfo from '@shared/components/detail-eventinfo/detail-info';
+import DetailHeader from '@shared/components/detail-header/detail-header';
+import Thumbnail from '@shared/components/Thumbnail/Thumbnail';
+
+const InfoData = {
+  subTitle: '디지털미디어디자인학과',
+  title: '진짜 맛집',
+  time: '15:00 ~ 16:00',
+  location: '20번',
+  description:
+    ' 맛있는 피자 치킨 돈까스 피자나라치킨공주에서 공급받아 판매중입니다.',
+};
+const BoothInfo = () => {
+  return (
+    <div>
+      <DetailHeader
+        subTitle={InfoData.subTitle}
+        title={InfoData.title}
+      />
+      <Thumbnail
+        src=""
+        alt=""
+        type="square_lg"
+      />
+      <DetailInfo
+        time="운영시간"
+        timevalue={InfoData.time}
+        location="부스위치"
+        locationvalue={InfoData.location}
+      />
+      <DetailDescription
+        title="부스 소개"
+        description={InfoData.description}
+      />
+    </div>
+  );
+};
+
+export default BoothInfo;
