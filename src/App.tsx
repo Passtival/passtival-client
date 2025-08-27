b385af9763193ccfa4f116864ddbfbaee625f3ee
jeonghoon11 <jih361@naver.com>
2025-08-27 15:42:33 +0900

    feat: global-error-boundary 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
+import GlobalErrorBoundary from '@router/global-error-boundary';
+
 import { rootStyle } from '@shared/styles/global.css';
 import ThemeProvider from '@shared/styles/theme-provider';
 import { queryClient } from '@shared/utils/query-client';
@@ -10,12 +12,14 @@ import { router } from './router/router';
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider className={rootStyle}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <GlobalErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider className={rootStyle}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </GlobalErrorBoundary>
   );
 }
 
