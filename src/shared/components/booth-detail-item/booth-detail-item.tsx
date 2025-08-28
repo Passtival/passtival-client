5c28af89f6aeb497814ed1ff8df675b50300a3ca
h2st0ry <yeonhee040512@gmail.com>
2025-08-29 01:04:22 +0900

    fix: props 명 수정

--- src/shared/components/booth-detail-item/booth-detail-item.tsx
+++ src/shared/components/booth-detail-item/booth-detail-item.tsx
@@ -2,18 +2,18 @@ import * as style from './booth-detail-item.css';
 import Thumbnail from '../Thumbnail/Thumbnail';
 
 interface BoothDetailItemProps {
-  itemName: string;
-  itemInfo: string;
-  itemImg: string;
-  itemAlt: string;
+  name: string;
+  info: string;
+  imgSrc: string;
+  alt: string;
   price?: number | string;
 }
 
 const BoothDetailItem = ({
-  itemName,
-  itemInfo,
-  itemImg,
-  itemAlt,
+  name,
+  info,
+  imgSrc,
+  alt,
   price,
 }: BoothDetailItemProps) => {
   const hasPrice = price !== undefined && price !== null && price !== '';
@@ -24,17 +24,17 @@ const BoothDetailItem = ({
       className={style.boothDetailItem}
     >
       <div className={style.boothText}>
-        <div className={style.boothItemName}>{itemName}</div>
+        <div className={style.boothItemName}>{name}</div>
 
         {hasPrice && <div className={style.boothPrice}>{price} 원</div>}
 
-        <p className={style.boothInfo}>{itemInfo}</p>
+        <p className={style.boothInfo}>{info}</p>
       </div>
       <div>
         <Thumbnail
-          src={itemImg}
+          src={imgSrc}
           type="square_sm"
-          alt={itemAlt}
+          alt={alt}
         />
       </div>
     </article>
