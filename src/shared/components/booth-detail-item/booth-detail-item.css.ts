7d34fc32f42a48d9cc6b57fa207c86aae1396e56
h2st0ry <yeonhee040512@gmail.com>
2025-08-28 17:17:39 +0900

    feat: booth-detail-item 생성

---
+++ src/shared/components/booth-detail-item/booth-detail-item.css.ts
@@ -0,0 +1,38 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@shared/styles';
+
+export const boothDetailItem = style({
+  display: 'flex',
+  width: '37.5rem',
+  height: '13.8rem',
+  padding: '1.9rem 3rem',
+  gap: '3.8rem',
+  wordBreak: 'keep-all',
+});
+
+export const boothText = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'left',
+});
+
+export const boothItemName = style({
+  ...themeVars.fontStyles.title_b_20,
+});
+
+export const boothPrice = style({
+  ...themeVars.fontStyles.title_b_16,
+  color: themeVars.color.main_blue,
+});
+
+export const boothInfo = style({
+  display: 'inline-grid',
+  gap: '0.6rem 0',
+  ...themeVars.fontStyles.body1_r_15,
+  color: themeVars.color.gray_600,
+});
+
+export const boothImg = style({
+  display: 'flex',
+});
--- src/shared/components/booth-detail-item/booth-detail-item.tsx
+++ src/shared/components/booth-detail-item/booth-detail-item.tsx
@@ -1,13 +1,11 @@
-//import * as style from './booth-detail-item.css';
-
-//import React from "react";
+import * as style from './booth-detail-item.css';
 
 export type BoothCategory = 'menu' | 'experience';
 
 type BaseProps = {
   itemName: string;
   itemInfo?: string;
-  className?: string;
+  img: string;
 };
 
 type MenuProps = BaseProps & {
@@ -26,27 +24,29 @@ const formatPrice = (p: number | string) =>
   typeof p === 'number' ? p.toLocaleString('ko-KR') : p;
 
 export default function BoothDetailItem(props: BoothDetailItemProps) {
-  const { itemName, itemInfo, className } = props;
+  const { itemName, itemInfo, img } = props;
   const isMenu = props.category === 'menu';
 
   return (
     <article
-      className={className}
       aria-label="부스 상세 아이템"
+      className={style.boothDetailItem}
     >
-      <div className="booth-detail-item rounded-xl border bg-white p-5">
-        <h3 className="title text-2xl font-extrabold">{itemName}</h3>
+      <div className={style.boothText}>
+        <div className={style.boothItemName}>{itemName}</div>
 
         {isMenu && (
-          <div className="price mt-1 text-lg font-bold">
-            {formatPrice(props.price)} 원
-          </div>
+          <div className={style.boothPrice}>{formatPrice(props.price)} 원</div>
         )}
 
-        {itemInfo && (
-          <p className="info mt-2 text-slate-500 leading-relaxed">{itemInfo}</p>
-        )}
+        {itemInfo && <p className={style.boothInfo}>{itemInfo}</p>}
       </div>
+      <img
+        className={style.boothImg}
+        src={img}
+        width={100}
+        height={100}
+      />
     </article>
   );
 }
