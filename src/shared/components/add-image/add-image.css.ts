b8ebef4a22c06dbfe6c6c39bd73f06c898d2496c
winchoose <tmdxor741@naver.com>
2025-08-29 17:04:43 +0900

    feat: 분실물 등록페이지 addImage 컴포넌트 구현

---
+++ src/shared/components/add-image/add-image.css.ts
@@ -0,0 +1,55 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@shared/styles';
+
+export const container = style({
+  display: 'flex',
+  gap: '1rem',
+  height: '9.6rem',
+  padding: '2.4rem 2.4rem 1rem 2.4rem',
+});
+
+export const addButton = style({
+  width: '6rem',
+  height: '6rem',
+  border: `1px solid ${themeVars.color.gray_400}`,
+  borderRadius: '5px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+});
+export const countText = style({
+  marginTop: '0.25rem',
+  fontSize: '0.75rem',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '2px',
+});
+
+export const current = style({
+  color: themeVars.color.main_blue,
+});
+
+export const max = style({
+  color: themeVars.color.gray_400,
+});
+
+export const uploadContainer = style({
+  position: 'relative',
+  width: '6rem',
+  height: '6rem',
+});
+
+export const removeButton = style({
+  position: 'absolute',
+  top: '0.3rem',
+  right: '0.3rem',
+  backgroundColor: themeVars.color.gray_600,
+  borderRadius: '42px',
+  width: '1rem',
+  height: '1rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
---
+++ src/shared/components/add-image/add-image.tsx
@@ -0,0 +1,63 @@
+import { IcSvgCamera, IcSvgX } from '@shared/icons';
+import { themeVars } from '@shared/styles';
+
+import * as styles from './add-image.css';
+
+interface AddImageProps {
+  value?: string;
+  onChange: (file?: File) => void;
+  disabled?: boolean;
+}
+const AddImage = ({ value, onChange, disabled }: AddImageProps) => {
+  const current = value ? 1 : 0;
+  const max = 1;
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    onChange(e.target.files[0]);
+    e.target.value = '';
+  };
+  const handleRemove = () => {
+    onChange(undefined);
+  };
+  return (
+    <div className={styles.container}>
+      <label className={styles.addButton}>
+        <IcSvgCamera
+          width="1.5rem"
+          height="1.33rem"
+        />
+        <input
+          type="file"
+          accept="image/*"
+          hidden
+          disabled={disabled}
+          onChange={handleFileChange}
+        />
+        <div className={styles.countText}>
+          <p className={styles.current}>{current}</p>/
+          <p className={styles.max}>{max}</p>
+        </div>
+      </label>
+      {value && (
+        <div className={styles.uploadContainer}>
+          <img
+            src={value}
+            alt="uploaded"
+          />
+          <button
+            onClick={handleRemove}
+            className={styles.removeButton}
+          >
+            <IcSvgX
+              width="0.6rem"
+              height="0.6rem"
+              color={themeVars.color.bg_white}
+            />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+export default AddImage;
