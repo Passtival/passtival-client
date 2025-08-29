a727d05d1584d76716c74dfd841cbc52bfaf54e3
twossu <nadao202@naver.com>
2025-08-30 04:02:04 +0900

    feat: 체크 했을시 버튼 활성화

--- src/pages/blind-match/blind-match.tsx
+++ src/pages/blind-match/blind-match.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+
+import AgreementOption from '@shared/components/agreement-option/agreement-option.tsx';
 import Button from '@shared/components/button/button';
 import Title from '@shared/components/title/title';
 import { IcSvgCaution } from '@shared/icons';
 
 import * as styles from './blind-match.css.ts';
-import Consent from './components/consent/consent';
 import UseInfoForm from './components/use-info-form/use-info-form';
 import { BLIND_MATCH_TEXT } from './constants/blind-match-text';
 
 const BlindMatch = () => {
+  const [agreed, setAgreed] = useState(false);
+  const handleConsentChange = (isAgreed: boolean) => {
+    setAgreed(isAgreed);
+  };
+
   return (
     <>
       <div className={styles.title}>
@@ -16,12 +23,15 @@ const BlindMatch = () => {
           subTitle={BLIND_MATCH_TEXT.SUBTITLE}
         />
       </div>
+
       <div className={styles.container}>
         <div className={styles.textcontainer}>
           <p className={styles.message}>{BLIND_MATCH_TEXT.MESSAGE}</p>
           <p className={styles.time}>{BLIND_MATCH_TEXT.TIME}</p>
         </div>
+
         <UseInfoForm />
+
         <div className={styles.attention}>
           <IcSvgCaution
             width={12}
@@ -29,7 +39,16 @@ const BlindMatch = () => {
           />
           {BLIND_MATCH_TEXT.ATTENTION}
         </div>
-        <Consent />
+
+        <div className={styles.agreementoption}>
+          <AgreementOption
+            label={BLIND_MATCH_TEXT.CONSENT}
+            navigateTo=""
+            checked={agreed}
+            onChange={handleConsentChange}
+          />
+        </div>
+
         <Button onClick={() => {}}>번호팅 신청하기</Button>
 
         <div className={styles.notice}>
--- src/pages/blind-match/components/use-info-form/use-info-form.tsx
+++ src/pages/blind-match/components/use-info-form/use-info-form.tsx
@@ -15,6 +15,7 @@ const UseInfoForm = () => {
   const [name, setName] = useState('');
   const [instaid, setInstaid] = useState('');
   const [phone, setPhone] = useState('');
+  const [selectedGender, setSelectedGender] = useState('');
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
@@ -24,6 +25,10 @@ const UseInfoForm = () => {
   };
   const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhone(e.target.value);
+  };
+
+  const handleGenderChange = (gender: string) => {
+    setSelectedGender(gender);
   };
 
   return (
@@ -46,14 +51,14 @@ const UseInfoForm = () => {
       <div className={styles.chip}>
         <Chip
           label="여성"
-          selected={false}
-          onChange={() => {}}
+          selected={selectedGender === '여성'}
+          onChange={() => handleGenderChange('여성')}
           size="lg"
         />
         <Chip
           label="남성"
-          selected={false}
-          onChange={() => {}}
+          selected={selectedGender === '남성'}
+          onChange={() => handleGenderChange('남성')}
           size="lg"
         />
       </div>
