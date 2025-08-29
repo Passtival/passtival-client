c8f21bc0f4e0dfe956f7e4089968a8fbcccf8b84
winchoose <tmdxor741@naver.com>
2025-08-29 20:23:10 +0900

    Feat: 분실물 등록페이지 addImage 컴포넌트 구현 (#112)
    * feat: 분실물 등록페이지 addImage 컴포넌트 구현
    
    * fix: 분실물 등록페이지 addImage 컴포넌트 구현/ 이미지 넘치는거 수정
    
    * fix: 분실물 등록페이지 addImage 컴포넌트 구현/ 코리내용 수정
    
    * fix: 분실물 등록페이지 addImage 컴포넌트 구현/ 코리내용 수정
    
    * fix: 분실물 등록페이지 addImage 컴포넌트 구현/ 코리내용 수정

--- src/pages/lost-items/lost-items.tsx
+++ src/pages/lost-items/lost-items.tsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
+import AddImage from '@shared/components/add-image/add-image';
+
 const LostItems = () => {
-  return <div>LostItems</div>;
+  const [image, setImage] = useState<string>();
+
+  const handleChange = (file?: File) => {
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setImage(url);
+    } else {
+      if (image) URL.revokeObjectURL(image);
+      setImage(undefined);
+    }
+  };
+  return (
+    <div>
+      <AddImage
+        src={image}
+        onFileChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default LostItems;
