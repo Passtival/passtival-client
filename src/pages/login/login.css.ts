21c367c3c30c6d73d69bf3cba49ca5b39fc70515
jeonghoon11 <jih361@naver.com>
2025-08-29 14:36:04 +0900

    fix: 주석 제거

--- src/pages/login/login.css.ts
+++ src/pages/login/login.css.ts
@@ -6,20 +6,20 @@ export const container = style({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  justifyContent: 'center', // body를 세로 중앙에 고정
+  justifyContent: 'center',
   flex: 1,
   textAlign: 'center',
-  position: 'relative', // 버튼 위치 제어 위해 relative
+  position: 'relative',
 });
 
 export const body = style({
   display: 'flex',
   flexDirection: 'column',
-  gap: '1.8rem', // 제목-설명 간격
+  gap: '1.8rem',
 });
 
 export const kakaoButton = style({
-  marginTop: '17.1rem', // body 기준으로 여백
+  marginTop: '17.1rem',
 });
 
 export const title = style({
