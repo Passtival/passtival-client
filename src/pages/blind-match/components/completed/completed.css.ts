7b99e5231ae6c4dc94a9e96721b466a67043d7ae
twossu <nadao202@naver.com>
2025-08-31 03:59:10 +0900

    feat: completed완성

---
+++ src/pages/blind-match/components/completed/completed.css.ts
@@ -0,0 +1,45 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@shared/styles';
+
+export const title = style({
+  height: '7.3rem',
+  padding: '1.3rem 2.4rem 0 2.4rem',
+});
+export const container = style({
+  display: 'grid',
+  placeItems: 'center',
+});
+export const completedContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '14.5rem 0 15rem 0',
+});
+export const endContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '14.5rem 0 16.5rem 0',
+});
+
+export const message = style({
+  ...themeVars.fontStyles.title_b_16,
+  color: themeVars.color.main_blue,
+});
+export const time = style({
+  ...themeVars.fontStyles.body1_r_15,
+  color: themeVars.color.gray_900,
+});
+export const caution = style({
+  ...themeVars.fontStyles.caption2_m_12,
+  color: themeVars.color.gray_900,
+});
+export const notice = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.3rem',
+  paddingTop: '0.6rem',
+  ...themeVars.fontStyles.button_r_12,
+  color: themeVars.color.gray_900,
+});
---
+++ src/pages/blind-match/components/completed/completed.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react';
+
+import Button from '@shared/components/button/button';
+import Title from '@shared/components/title/title';
+import { IcSvgCaution } from '@shared/icons';
+
+import * as styles from './completed.css';
+
+const Completed = () => {
+  const [isMatchingEnd, setIsMatchingEnd] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkMatchingStatus = () => {
+      const now = new Date();
+      const endTime = new Date();
+      endTime.setHours(13, 51, 0, 0);
+
+      if (now > endTime) {
+        setIsMatchingEnd(true);
+      }
+    };
+
+    checkMatchingStatus();
+
+    const intervalId = setInterval(checkMatchingStatus, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <>
+      <div className={styles.title}>
+        <Title
+          mainTitle="번호팅"
+          subTitle="랜덤 매칭으로 새로운 인연을 만들어주는 서비스"
+        />
+      </div>
+      <div className={styles.container}>
+        {isMatchingEnd ? (
+          <>
+            <div className={styles.endContainer}>
+              <p className={styles.message}>
+                1일차 매칭 신청이 마감되었습니다.
+              </p>
+              <p className={styles.time}>[신청 가능 시간: 00:00 ~ 17:30]</p>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.completedContainer}>
+              <p className={styles.message}>
+                1일차 매칭 신청이 완료되었습니다.
+              </p>
+              <p className={styles.time}>[신청 가능 시간: 00:00 ~ 17:30]</p>
+              <p className={styles.caution}>
+                ※ 성비 불균형으로 인해 매칭이 이루어지지 않을 수 있습니다.
+              </p>
+            </div>
+          </>
+        )}
+
+        <Button
+          disabled={true}
+          onClick={() => {}}
+        >
+          매칭 결과 대기중
+        </Button>
+        <div className={styles.notice}>
+          <IcSvgCaution style={{ width: 12, height: 12 }} />
+          <p>18시에 매칭결과가 제공됩니다.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Completed;
