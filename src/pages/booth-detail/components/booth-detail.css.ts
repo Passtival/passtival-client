7c90dcc4783356f1c9fed224c1e04789c4b18af2
h2st0ry <yeonhee040512@gmail.com>
2025-08-31 01:40:30 +0900

    fix: text p 태그 삭제에 따른 css 삭제

--- src/pages/booth-detail/components/booth-detail.css.ts
+++ src/pages/booth-detail/components/booth-detail.css.ts
@@ -1,15 +1,5 @@
 import { style } from '@vanilla-extract/css';
 
-import { themeVars } from '@shared/styles';
-
-export const text = style({
-  width: '100%',
-  height: '5.5rem',
-  padding: '2.2rem 0rem 0rem 2.5rem',
-  ...themeVars.fontStyles.head_eb_20,
-  color: themeVars.color.gray_900,
-});
-
 export const boothDetailItemContainer = style({
   padding: '1.9rem 3rem 1.3rem 3rem',
 });
