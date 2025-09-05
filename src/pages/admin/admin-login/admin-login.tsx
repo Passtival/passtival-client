295befb52b1e203addaa2fa63ca62e888f91d72d
jeonghoon11 <jih361@naver.com>
2025-09-05 23:26:50 +0900

    feat: 관리자 로그인 페이지 구현

---
+++ src/pages/admin/admin-login/admin-login.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { routePath } from '@router/path';
+
+import { TITLE } from '@pages/admin/constants/TITLE';
+
+import Button from '@shared/components/button/button';
+import Input from '@shared/components/input/input';
+
+import * as styles from './admin-login.css';
+
+const AdminLogin = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+
+  const handleAdminLogin = () => {
+    navigate(routePath.ADMIN_MAIN);
+  };
+
+  return (
+    <div>
+      <div className={styles.contentWrapper}>
+        <p className={styles.title}>{TITLE.MAIN}</p>
+        <p className={styles.subTitle}>{TITLE.SUB}</p>
+      </div>
+      <div className={styles.loginContainer}>
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호"
+        />
+        <Button
+          children="로그인"
+          onClick={handleAdminLogin}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AdminLogin;
