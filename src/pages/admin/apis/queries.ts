6509140960c88ff150c2be5263513eac1363d31f
jeonghoon11 <jih361@naver.com>
2025-09-06 02:49:22 +0900

    refactor: queries파일 순서 변경

--- src/pages/admin/apis/queries.ts
+++ src/pages/admin/apis/queries.ts
@@ -10,6 +10,15 @@ import { api } from '@shared/apis/config/instance';
 import { ADMIN_QUERY_KEY } from '@shared/apis/keys/query-key';
 import { adminAuthService } from '@shared/auth/services/admin-auth-service';
 
+export const ADMIN_MUTATION_OPTIONS = {
+  ADMIN_LOGIN: () =>
+    mutationOptions({
+      mutationKey: ADMIN_QUERY_KEY.ADMIN_LOGIN(),
+      mutationFn: (authKey: string) =>
+        adminAuthService.loginWithAuthKey(authKey),
+    }),
+};
+
 export const postAdminLogin = async (
   body: AdminLoginRequest,
 ): Promise<AdminLoginResponse> => {
@@ -18,13 +27,4 @@ export const postAdminLogin = async (
     body,
   );
   return data;
-};
-
-export const ADMIN_MUTATION_OPTIONS = {
-  ADMIN_LOGIN: () =>
-    mutationOptions({
-      mutationKey: ADMIN_QUERY_KEY.ADMIN_LOGIN(),
-      mutationFn: (authKey: string) =>
-        adminAuthService.loginWithAuthKey(authKey),
-    }),
 };
