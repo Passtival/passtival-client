9b27793e42b6eda1abadf66a969b4d2d51e19a00
twossu <nadao202@naver.com>
2025-09-06 19:52:18 +0900

    feat: css 추가

--- src/pages/admin/ticket-drawing/drawing-component.tsx
+++ src/pages/admin/ticket-drawing/drawing-component.tsx
@@ -1,5 +1,19 @@
+import Button from '@shared/components/button/button';
+
+import * as styles from './drawing-component.css';
+
 const DrawingComponent = () => {
-  return <>안녕하세요</>;
+  const handleButtonClick = () => {
+    console.log('응모권 추첨 버튼이 클릭되었습니다!');
+  };
+  return (
+    <>
+      <p className={styles.text}>응모권 추첨</p>
+      <div className={styles.container}>
+        <Button onClick={handleButtonClick}>응모권 추첨</Button>
+      </div>
+    </>
+  );
 };
 
 export default DrawingComponent;
