130aa9682237061a0862fe6a5d272666cb943517
twossu <nadao202@naver.com>
2025-09-06 19:40:03 +0900

    feat: 라우팅 설정

---
+++ src/pages/admin/ticket-drawing/drawing-component.tsx
@@ -0,0 +1,5 @@
+const DrawingComponent = () => {
+  return <>안녕하세요</>;
+};
+
+export default DrawingComponent;
---
+++ src/pages/admin/ticket-drawing/ticket-drawing.tsx
@@ -0,0 +1,36 @@
+import Tab from '@shared/components/tab/tab';
+
+import DrawingComponent from './drawing-component';
+
+const BlindMatch = () => {
+  const initialDay = '1';
+
+  return (
+    <>
+      <Tab.Container initialValue={initialDay}>
+        <Tab.List>
+          <Tab.Item value="1">1일차</Tab.Item>
+          <Tab.Item value="2">2일차</Tab.Item>
+          <Tab.Item value="3">3일차</Tab.Item>
+          <Tab.Item value="4">Premium</Tab.Item>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel value="1">
+            <DrawingComponent />
+          </Tab.Panel>
+          <Tab.Panel value="2">
+            <DrawingComponent />
+          </Tab.Panel>
+          <Tab.Panel value="3">
+            <DrawingComponent />
+          </Tab.Panel>
+          <Tab.Panel value="4">
+            <DrawingComponent />
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Container>
+    </>
+  );
+};
+
+export default BlindMatch;
