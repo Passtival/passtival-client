01a54b1fdb502c357a1a8b89795391dee17606f5
jeonghoon11 <jih361@naver.com>
2025-09-08 10:10:57 +0900

    refactor: 티켓 페이지 카카오 로그인 라우터 설정

--- src/router/routes/protected-route.tsx
+++ src/router/routes/protected-route.tsx
@@ -11,7 +11,10 @@ export function ProtectedRoute() {
   if (!isAuthenticated) {
     const currentPath = location.pathname;
 
-    if (currentPath === routePath.BLIND_MATCH) {
+    if (
+      currentPath === routePath.BLIND_MATCH ||
+      currentPath === routePath.TICKET
+    ) {
       return (
         <Navigate
           to={routePath.LOGIN}
