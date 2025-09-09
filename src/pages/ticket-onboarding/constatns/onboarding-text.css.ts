1f9dbb6471a607a9315f20aa845e060f1957c281
twossu <nadao202@naver.com>
2025-09-09 18:34:18 +0900

    fix: 응모권 온보딩 수정

--- src/pages/ticket-onboarding/constatns/onboarding-text.css.ts
+++ src/pages/ticket-onboarding/constatns/onboarding-text.css.ts
@@ -2,21 +2,28 @@ import { style } from '@vanilla-extract/css';
 
 import { themeVars } from '@shared/styles';
 
-export const container = style({
-  paddingBottom: '0.8rem',
+export const ticketImg = style({
+  display: 'flex',
+  alignItems: 'center',
+
+  justifyContent: 'center',
+  height: '20rem',
+  width: '100%',
+  margin: '3.9rem 0 3.7rem 0',
 });
 
 export const title = style({
   ...themeVars.fontStyles.title_b_22,
   color: themeVars.color.main_yellow,
-  paddingBottom: '0.6rem',
+  padding: '3.4rem 0 0.6rem ',
 });
 
-export const section = style({ gap: '0.4rem', paddingBottom: '0.8rem' });
+export const section = style({});
 
 export const header = style({
   ...themeVars.fontStyles.caption2_m_15,
   color: themeVars.color.main_yellow,
+  paddingBottom: '0.4rem',
 });
 
 export const nottitle = style({
@@ -26,6 +33,8 @@ export const nottitle = style({
 export const description = style({
   ...themeVars.fontStyles.caption2_m_12,
   color: themeVars.color.bg_white,
+
+  paddingBottom: '0.8rem',
 });
 
 export const point = style({
