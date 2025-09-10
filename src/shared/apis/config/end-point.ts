8c7a4315b78b3ee23bff8dde18cbcd4acc956a23
jeonghoon11 <jih361@naver.com>
2025-09-10 13:53:02 +0900

    feat: 카카오 로그인 API 엔드포인트 추가 및 로그인 버튼 리다이렉트 수정

--- src/shared/apis/config/end-point.ts
+++ src/shared/apis/config/end-point.ts
@@ -9,6 +9,7 @@ export const END_POINT = {
   ADMIN_RAFFLE_AUTH_KEY: '/api/admin/raffle/authentication-key',
   AUTH_REFRESH: '/api/auth/refresh',
   MEMBER_LEVEL_UP: '/api/member/level-up',
+  MEMBER_LOGIN: '/api/member/login/kakao',
   ADMIN_RAFFLE_DAY: '/api/admin/raffle/{day}',
   ADMIN_RAFFLE_WINNERS: '/api/admin/raffle/{day}',
 } as const;
