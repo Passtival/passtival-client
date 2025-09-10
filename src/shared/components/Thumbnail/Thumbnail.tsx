7a8f5a20c4db44999c24100b5c653d89d7efcff1
twossu <nadao202@naver.com>
2025-09-10 16:39:44 +0900

    feat: test 저장

--- src/shared/components/Thumbnail/Thumbnail.tsx
+++ src/shared/components/Thumbnail/Thumbnail.tsx
@@ -5,13 +5,20 @@ interface ThumbnailProps {
   alt?: string;
   type: 'square_sm' | 'square_md' | 'square_lg';
 }
+
+const NO_IMAGE_PLACEHOLDER = '/public/carousel1.jpg';
+
 const Thumbnail = ({ src, alt, type }: ThumbnailProps) => {
+  const imageSrc = src || NO_IMAGE_PLACEHOLDER;
+
+  const imageAlt = alt || '썸네일 이미지';
+
   return (
     <div className={styles.container}>
       <img
         className={styles.img({ type })}
-        src={src}
-        alt={alt}
+        src={imageSrc}
+        alt={imageAlt}
       />
     </div>
   );
