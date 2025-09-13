d5580534de231c71ba45331f071712ccb492f33f
winchoose <tmdxor741@naver.com>
2025-09-14 00:00:39 +0900

    feat: 개인정보 동의 TopNavigation 추가

--- src/pages/info-share/info-share.tsx
+++ src/pages/info-share/info-share.tsx
@@ -4,34 +4,40 @@ import {
   CONSENT_TITLE,
 } from '@pages/info-share/constants/INFO_SHARE_CONSENT';
 
+import TopNavigation from '@shared/components/top-navigation/top-navigation';
+
 import * as styles from './info-share.css';
 
 const InfoShare = () => {
   return (
-    <article className={styles.container}>
-      <header>
-        <h1 className={styles.title}>{CONSENT_TITLE}</h1>
-        <p className={styles.subtitle}>{CONSENT_SUBTITLE}</p>
-      </header>
+    <>
+      <TopNavigation title="개인정보" />
+      <article className={styles.container}>
+        <div></div>
+        <header>
+          <h1 className={styles.title}>{CONSENT_TITLE}</h1>
+          <p className={styles.subtitle}>{CONSENT_SUBTITLE}</p>
+        </header>
 
-      <ol>
-        {CONSENT_CONTENT.map(({ id, TITLE, TEXT }) => (
-          <li key={id}>
-            <h2 className={styles.itemTitle}>{TITLE}</h2>
-            <ul>
-              {TEXT.map((text, i) => (
-                <li
-                  className={styles.text}
-                  key={i}
-                >
-                  {text}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ol>
-    </article>
+        <ol>
+          {CONSENT_CONTENT.map(({ id, TITLE, TEXT }) => (
+            <li key={id}>
+              <h2 className={styles.itemTitle}>{TITLE}</h2>
+              <ul>
+                {TEXT.map((text, i) => (
+                  <li
+                    className={styles.text}
+                    key={i}
+                  >
+                    {text}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ol>
+      </article>
+    </>
   );
 };
 
