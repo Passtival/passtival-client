fbc3a544785448d1477405c65744f4a6d29d23f9
twossu <nadao202@naver.com>
2025-09-15 23:42:22 +0900

    fix: card css 저장

--- src/shared/components/card/card.css.ts
+++ src/shared/components/card/card.css.ts
@@ -3,25 +3,36 @@ import { recipe } from '@vanilla-extract/recipes';
 
 import { themeVars } from '@shared/styles';
 
-export const container = style({
-  display: 'flex',
-  justifyContent: 'space-between',
+export const container = recipe({
+  base: {
+    display: 'flex',
+    justifyContent: 'space-between',
 
-  borderRadius: '10px',
-  width: '100%',
-  height: '13.3rem',
-  backgroundColor: themeVars.color.gray_500_40,
-  gap: '0.5rem',
-});
-
-export const titleContainer = style({
-  display: 'flex',
-  flexDirection: 'column',
+    borderRadius: '10px',
+    width: '100%',
+    height: '13.3rem',
+    backgroundColor: themeVars.color.gray_500_40,
+    gap: '1rem',
+  },
+  variants: {
+    type: {
+      sm: {
+        padding: '1.4rem 1.1rem 0 1.5rem',
+      },
+      lg: {
+        padding: '1.2rem 1.6rem 1.6rem 1.6rem',
+      },
+    },
+  },
 });
 
 export const content = style({
   display: 'flex',
   flexDirection: 'column',
+  width: '100%',
+  flex: '1 1 auto',
+  alignItems: 'flex-start',
+  minWidth: 0,
 });
 
 export const title = recipe({
@@ -31,12 +42,17 @@ export const title = recipe({
       sm: {
         ...themeVars.fontStyles.title_b_18,
         color: themeVars.color.bg_white,
-        padding: '1.4rem 1.1rem 0 1.5rem',
       },
       lg: {
+        width: '100%',
+        paddingTop: '1.1rem',
+
         ...themeVars.fontStyles.title_b_20,
         color: themeVars.color.main_yellow,
-        padding: '2.3rem 1.1rem 0 1.6rem',
+        WebkitLineClamp: 1,
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
       },
     },
   },
@@ -45,13 +61,6 @@ export const title = recipe({
 export const assignee = style({
   ...themeVars.fontStyles.title_b_14,
   color: themeVars.color.main_yellow,
-  padding: '0 1.1rem 0.5rem 1.5rem',
-});
-
-export const descriptionContainer = style({
-  display: 'flex',
-  flexDirection: 'column',
-  height: '1.7rem',
 });
 
 export const description = recipe({
@@ -59,7 +68,7 @@ export const description = recipe({
     maxWidth: '16rem',
     display: '-webkit-box',
     WebkitBoxOrient: 'vertical',
-    WebkitLineClamp: 2,
+    WebkitLineClamp: 3,
     overflow: 'hidden',
   },
   variants: {
@@ -67,24 +76,17 @@ export const description = recipe({
       sm: {
         ...themeVars.fontStyles.caption2_m_12,
         color: themeVars.color.gray_200,
-        padding: '0 1.1rem 3.4rem 1.5rem',
+        paddingTop: '0.5rem',
       },
       md: {
         ...themeVars.fontStyles.caption2_m_12,
         color: themeVars.color.gray_400,
-        padding: '0 1.1rem 3.4rem 1.5rem',
+        paddingTop: '0.5rem',
       },
       lg: {
         ...themeVars.fontStyles.body1_r_15,
         color: themeVars.color.gray_400,
-        padding: '0 1.1rem 4.1rem 1.6rem',
       },
     },
   },
-});
-
-export const img = style({
-  zIndex: themeVars.zIndex.modal,
-  padding: '1.2rem 1.6rem 1.6rem 0',
-  display: 'flex',
 });
